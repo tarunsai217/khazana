e261aba520bb3a26a9b768c61529b3aad32f8813
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2024-11-22 14:15:35 +0530

    fix toggle theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths according to your file structure
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
