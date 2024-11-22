4b4f4b0cfb2fe42bb31bed7737585f6eb46c14ca
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2024-11-22 13:08:07 +0530

    sample react app using vite

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths according to your file structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
